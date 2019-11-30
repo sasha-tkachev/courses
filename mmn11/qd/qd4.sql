d4f87b41373be5b31669cc7c1727b09ae19d9614
Nicodemes <sasha64sasha@gmail.com>
2019-11-30 15:56:59 +0200

    Fix qd4

--- mmn11/qd/qd4.sql
+++ mmn11/qd/qd4.sql
@@ -1,7 +1,6 @@
 -- qd4
 select Department.did, Department.dname
 from Budget natural join Department
--- '20/11/2019' is the current date as per the forum discussion
 where Budget.byear >= (date_part('year', current_date) - 5 )
-group by Department.did, Budget.did
+group by Department.did
 having ( sum(Budget.budget) < 2500000);
