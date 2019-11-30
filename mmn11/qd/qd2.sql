2bec5d8ca55189278dfb034cc8fa23d832041485
Nicodemes <sasha64sasha@gmail.com>
2019-11-30 15:49:34 +0200

    Fix qd2

--- mmn11/qd/qd2.sql
+++ mmn11/qd/qd2.sql
@@ -1,6 +1,7 @@
 -- qd2
 select Employee.ename, Employee.salary
-from Employee natural join Department natural join Onproject
-where Department.did = 1 and Onproject.pid in (
-	select pid from Project where pname = 'search'
-);
+from Employee, Onproject
+where 
+	Onproject.eid = Employee.eid and Employee.did = 1 and Onproject.pid in (
+		select pid from Project where pname = 'search'
+	);
