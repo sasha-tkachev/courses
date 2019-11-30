9a33b799fad6fc470694392a004aaa2d45d8121d
Nicodemes <sasha64sasha@gmail.com>
2019-11-30 15:32:58 +0200

    Fix make test

--- make_test.bat
+++ make_test.bat
@@ -1,7 +1,8 @@
 mkdir build
 type reset_state.sql > build/test_query.sql
 
-type qa.sql >> build/test_query.sql
-type qb.sql >> build/test_query.sql
-type qc.sql >> build/test_query.sql
-type "qd\qd%1.sql" >> "build\test_query.sql"
+type "mmn11\qa.sql" >> "build\test_query.sql"
+type "mmn11\qb.sql" >> "build\test_query.sql"
+type "mmn11\qc.sql" >> "build\test_query.sql"
+type "mmn11\qd\qd%1.sql" >> "build\test_query.sql"
+start notepad.exe "build\test_query.sql"
