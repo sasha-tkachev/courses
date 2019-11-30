91b8224e91103a4db1a71cca1252b11e459f2543
Nicodemes <sasha64sasha@gmail.com>
2019-11-30 16:35:13 +0200

    Add author

--- make.ps1
+++ make.ps1
@@ -1,1 +1,7 @@
+
+$FirstName = Read-Host -Prompt 'Input your first name'
+$LastName = Read-Host -Prompt 'Input your last name'
+$TazID = Read-Host -Prompt 'Input your TAZ-ID'
+"MMN11 By $FirstName $LastName ID $TazID"  | Out-File -FilePath ".\mmn11\README.txt"
 Compress-Archive -Path .\mmn11 -DestinationPath .\build\mmn11.zip
+Remove-Item ".\mmn11\README.txt"
