1cef4d0b07899d3bc5eb134fb3b6563bf3689e33
Nicodemes <sasha64sasha@gmail.com>
2019-11-30 15:52:53 +0200

    fix qd3

--- mmn11/qd/qd3.sql
+++ mmn11/qd/qd3.sql
@@ -2,4 +2,4 @@
 select Department.did, Department.dname, sum(Project.budget) 
 from Project natural join Department
 where Project.duedate > '20/11/2019' -- '20/11/2019' is the current date as per the forum discussion
-group by Department.did, Project.did;
+group by Department.did
